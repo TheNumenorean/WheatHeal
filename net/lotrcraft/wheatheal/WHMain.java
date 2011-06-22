f1827f2f3166df0b5303a766f5d5a5ba41cce20f
Francesco Macagno <fjmacagno1@yahoo.com>
2011-06-22 16:28:02 -0700

    first working version

---
+++ net/lotrcraft/wheatheal/WHMain.java
@@ -0,0 +1,29 @@
+package net.lotrcraft.wheatheal;
+
+import java.util.logging.Logger;
+import org.bukkit.event.Event;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.config.Configuration;
+
+public class WHMain extends JavaPlugin {
+
+	Configuration config;
+	public static int heal = 0;
+	Logger log = Logger.getLogger("minecraft");
+
+	public void onDisable() {
+		log.info("WheatHeal Disabled");
+	}
+
+	public void onEnable() {
+		config = this.getConfiguration();
+		this.getServer().getPluginManager().registerEvent(Event.Type.ENTITY_DAMAGE, new WHPlayerListener(), Event.Priority.High, this);
+		log.info("WheatHeal Enabled");
+		if (config.getInt("healAmt", 1) <= 0 || config.getInt("healAmt", 1) > 10){
+			config.setProperty("healAmt", 1);
+		}
+		config.save();
+		heal = config.getInt("healAmt", 1);
+	}
+
+}
---
+++ net/lotrcraft/wheatheal/WHPlayerListener.java
@@ -0,0 +1,26 @@
+package net.lotrcraft.wheatheal;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class WHPlayerListener implements Listener {
+	public void onEntityDamage(EntityDamageEvent event) {
+		if (event instanceof EntityDamageByEntityEvent){
+			EntityDamageByEntityEvent e = (EntityDamageByEntityEvent)event;
+			if (event.getEntity() instanceof Player && e.getDamager() instanceof Player){
+		        Player puncher = (Player)e.getDamager();
+		        Player punchee = (Player)e.getEntity();
+				if (puncher.getItemInHand().equals(Material.WHEAT)){
+					e.setCancelled(true);
+					puncher.getItemInHand().setAmount(puncher.getItemInHand().getAmount()-1);
+					punchee.setHealth(punchee.getHealth()+WHMain.heal);
+				}
+				
+			}
+		}
+	}
+
+}
