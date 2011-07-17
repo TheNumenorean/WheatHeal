693d6d681af9b49b82939a597a51b856f11ea623
Adrian Reid <devilred101@gmail.com>
2011-07-17 01:32:04 +0100

    Changed the logical operators when detecting what is in the puncher's hands

--- net/lotrcraft/wheatheal/Healer.java
+++ net/lotrcraft/wheatheal/Healer.java
@@ -7,20 +7,20 @@ public class Healer {
 	private ConfigHandler config = WHMain.config;
 	
 	// CONSTRUCTOR - Not in use
-	/*public void ConfigHandler() {
+	public void ConfigHandler() {
 		
-	}*/
+	}
 
 	public void healPlayer(Player player, int itemID) {
 		
 		switch (itemID) {
 		// Use the itemID passed to determine what the heal amount is
-		case 296: player.setHealth(player.getHealth() + config.amounts[0]); // WHEAT
-		case 319: player.setHealth(player.getHealth() + config.amounts[1]); // RAW_PORKCHOP
-		case 320: player.setHealth(player.getHealth() + config.amounts[2]); // COOKED_PORKCHOP
-		case 349: player.setHealth(player.getHealth() + config.amounts[3]); // RAW_FISH
-		case 350: player.setHealth(player.getHealth() + config.amounts[4]); // COOKED_FISH
-		case 297: player.setHealth(player.getHealth() + config.amounts[5]); // BREAD		
+		case 296: player.setHealth(player.getHealth() + config.amounts[0]); break;  // WHEAT
+		case 319: player.setHealth(player.getHealth() + config.amounts[1]); break;  // RAW_PORKCHOP
+		case 320: player.setHealth(player.getHealth() + config.amounts[2]); break;  // COOKED_PORKCHOP
+		case 349: player.setHealth(player.getHealth() + config.amounts[3]); break;  // RAW_FISH
+		case 350: player.setHealth(player.getHealth() + config.amounts[4]); break;  // COOKED_FISH
+		case 297: player.setHealth(player.getHealth() + config.amounts[5]); break;  // BREAD		
 		}
 		
 	}
--- net/lotrcraft/wheatheal/WHListener.java
+++ net/lotrcraft/wheatheal/WHListener.java
@@ -6,39 +6,25 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityListener;
 
 public class WHListener extends EntityListener{
-	
+
 	private Healer healer = WHMain.healer;;
-	
+
 	public void onEntityDamage(EntityDamageEvent event) {
-		
+
 		if (event instanceof EntityDamageByEntityEvent){
 			EntityDamageByEntityEvent e = (EntityDamageByEntityEvent)event;
-			
-			if (e.getEntity() instanceof Player && e.getDamager() instanceof Player){
-		        Player puncher = (Player)e.getDamager();
-		        Player punchee = (Player)e.getEntity();
-		        
-		        int itemID = punchee.getItemInHand().getTypeId();
-		        
-		        if (itemID == (296 | 319 | 320 | 349 | 350 | 297)){
-					event.setCancelled(true);
-					if (punchee.getHealth() < 20){
-						if (puncher.getItemInHand().getAmount() == 1){
-							puncher.setItemInHand(null);
-						}else {
-							puncher.getItemInHand().setAmount(puncher.getItemInHand().getAmount()-1);
-						}
-						//punchee.setHealth(punchee.getHealth()+WHMain.heal);
-						healer.healPlayer(punchee, punchee.getItemInHand().getTypeId());
-					}
-				// DEBUG ONLY CODE
-				} else {
-					WHMain.log.info("itemID:" + Integer.toString(itemID));
-				// END OF DEBUG ONLY CODE - COMMENT OUT ABOVE LINES 
-				}
+
+			if ((e.getEntity() instanceof Player) && (e.getDamager() instanceof Player)){
+				Player puncher = (Player)e.getDamager();
+				Player punchee = (Player)e.getEntity();
+				int itemID = punchee.getItemInHand().getTypeId();
 				
+				if (itemID == 296 || itemID == 297 || itemID == 319 || itemID == 320 || itemID == 349 || itemID == 350) {
+					WHMain.log.info("ItemID:" + itemID + " Punchee:" + punchee.getName()+ " Puncher:" + puncher.getName());
+					healer.healPlayer(punchee, itemID);
+				}
 			}
+
 		}
 	}
-
 }
