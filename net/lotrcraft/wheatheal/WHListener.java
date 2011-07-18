00cbf9258a147c8ece0792c296e0e2e827e1f5f6
Adrian Reid <devilred101@gmail.com>
2011-07-18 01:41:03 +0100

    Decreasing or removing the amount/item in puncher's hand seems to have been removed. Added it again.

--- net/lotrcraft/wheatheal/WHListener.java
+++ net/lotrcraft/wheatheal/WHListener.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityListener;
+import org.bukkit.inventory.ItemStack;
 
 public class WHListener extends EntityListener{
 
@@ -21,11 +22,20 @@ public class WHListener extends EntityListener{
 					int itemID = punchee.getItemInHand().getTypeId();
 
 					// Only perform the code if one of the itemID's is detected in the puncher's hand
-					if (itemID == 296 || itemID == 297 || itemID == 319 || itemID == 320 || itemID == 349 || itemID == 350) {
+					if (itemID == 296 || itemID == 297 || itemID == 319 || itemID == 320 || itemID == 349 || itemID == 350
+							|| itemID == 357 || itemID == 360 || itemID == 322) {
 						// DEBUG LINE BELOW - REMOVE ONCE TESTING IS COMPLETE
 						WHMain.log.info("Healer - ItemID:" + itemID + " Punchee:" + punchee.getName()+ " Puncher:" + puncher.getName());
 						// Cancel the event to prevent any damage being caused to the player being punched
 						event.setCancelled(true);
+						
+						// Decrease itemInHand amount by 1 or remove if the player only had 1 of the item
+						if (punchee.getItemInHand().getAmount() > 1) {
+							punchee.setItemInHand(new ItemStack(itemID, punchee.getItemInHand().getAmount() -1));
+						} else if (punchee.getItemInHand().getAmount() == 1) {
+							punchee.setItemInHand(null); // Unsure if this should be 'null' or 'new ItemStack(null)'
+						}
+											
 						// Finally call healPlayer in our healer class and pass in the punchee and the itemID
 						healer.healPlayer(punchee, itemID);
 					}
