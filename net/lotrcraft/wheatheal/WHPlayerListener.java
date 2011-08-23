c3760e07c778f087dcb8bee2fb980ad97447795e
Lathanael <101@abwesend.de>
2011-08-23 11:57:59 +0200

    Changed order of the if check

--- net/lotrcraft/wheatheal/WHPlayerListener.java
+++ net/lotrcraft/wheatheal/WHPlayerListener.java
@@ -13,7 +13,7 @@ public class WHPlayerListener extends PlayerListener {
 		Player player = event.getPlayer();
 		int item = player.getItemInHand().getTypeId();
 
-		if (event.getAction() == Action.RIGHT_CLICK_BLOCK && item == 296){ // If Right-Click with Wheat in hand continue
+		if (item == 296 && event.getAction() == Action.RIGHT_CLICK_BLOCK){ // If Right-Click with Wheat in hand continue
 
 			// Cancel the event, just so nothing bad can happen
 			event.setCancelled(true);
@@ -29,7 +29,7 @@ public class WHPlayerListener extends PlayerListener {
 				player.setItemInHand(null);
 			}
 		}
-		else if (event.getAction()== Action.RIGHT_CLICK_AIR && item == 296){ // If Right-Click with Wheat in hand continue
+		else if (item == 296 && event.getAction()== Action.RIGHT_CLICK_AIR){ // If Right-Click with Wheat in hand continue
 
 			// Cancel the event, just so nothing bad can happen
 			event.setCancelled(true);
