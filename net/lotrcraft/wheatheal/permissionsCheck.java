146a01bbdad3cb8bba6db609c3abb4f9c4ec82cc
Lathanael <101@abwesend.de>
2011-08-23 11:55:08 +0200

    Changed PermCheck again a little bit

--- net/lotrcraft/wheatheal/permissionsCheck.java
+++ net/lotrcraft/wheatheal/permissionsCheck.java
@@ -8,9 +8,11 @@ public class permissionsCheck {
 
 	public static boolean check(CommandSender sender, String permission) {
 
+		// If command was send via console return true, as console does not have any permission
 		if (sender instanceof ConsoleCommandSender) return true;
-		else if (Config.useBukkitPerms) { // If using PermissionsBukkit
-			Player player = (Player) sender;
+		Player player = (Player) sender;
+
+		if (Config.useBukkitPerms) { // If using PermissionsBukkit
 			if (player.hasPermission(permission)) { // Its good
 				return true;
 			}
@@ -20,16 +22,16 @@ public class permissionsCheck {
 		}
 		else { // If using Nijikokun's Permissions or t3hk0d3's PermissionsEx
 			if (WHMain.nijikoPermissions != null) { // Check that Permissions is enabled
-				if (WHMain.nijikoPermissions.has((Player)sender, permission) || sender.isOp()) { // Again you can change this node if you want
+				if (WHMain.nijikoPermissions.has(player, permission) || sender.isOp()) { // Again you can change this node if you want
 					return true;
 				}
 			}
 			else if (WHMain.permissionsEx != null) { // Check that Permissions is enabled
-				if (WHMain.permissionsEx.has((Player)sender, permission) || sender.isOp()) { // Again you can change this node if you want
+				if (WHMain.permissionsEx.has(player, permission) || sender.isOp()) { // Again you can change this node if you want
 					return true;
 				}
 			}
-			else if (sender.isOp()) { // If player doesn't have the permission but is an op
+			else if (player.isOp()) { // If player doesn't have the permission but is an op
 				return true;
 			}
 		}
