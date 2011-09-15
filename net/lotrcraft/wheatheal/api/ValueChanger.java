2f2cb49157768e0f494e0b12f1863c09b654c6e9
Francesco Macagno <Francesco@lotrcraft.net>
2011-09-15 16:29:42 -0700

    Added Small phrase to API

--- net/lotrcraft/wheatheal/api/ValueChanger.java
+++ net/lotrcraft/wheatheal/api/ValueChanger.java
@@ -6,10 +6,10 @@ import net.lotrcraft.wheatheal.WHMain;
 public class ValueChanger {
 
 	public boolean setEnabled(String food, boolean value){
-		return Config.confEditUse(food, value);
+		return Config.confEditUse("Foods." + food, value);
 	}
 	public boolean setHealvalue(String food, int value){
-		return Config.confEditAmount(food, value);
+		return Config.confEditAmount("Foods." + food, value);
 	}
 	public void reset(){
 		Config.confRestore(WHMain.config);
