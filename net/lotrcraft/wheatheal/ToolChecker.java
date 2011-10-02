fc2392b70c6edb30474fc8d1fb153e71ce4449e8
Lathanael <101@abwesend.de>
2011-10-02 11:48:38 +0200

    NPE fix in ToolChecker, BukkitDev Ticket #1

--- net/lotrcraft/wheatheal/ToolChecker.java
+++ net/lotrcraft/wheatheal/ToolChecker.java
@@ -5,8 +5,11 @@ import java.util.List;
 import net.lotrcraft.wheatheal.tools.Tool;
 
 public class ToolChecker {
+
 	public static Tool checkTool(int id) {
 		List<Tool> tools = Config.getToolList();
+		if (tools == null)
+			return null;
 		for (int y = 0; y < tools.size(); y++){
 			if (tools.get(y).getId() == id) {
 				return tools.get(y);
